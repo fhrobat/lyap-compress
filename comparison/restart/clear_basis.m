1b28d187f38d1da8dc5cb0dbaf58d4e8434637a5
Francesco Hrobat <100699189+fhrobat@users.noreply.github.com>
2026-07-16 10:35:14 +0100

    Delete comparison/restart directory

--- comparison/restart/clear_basis.m
+++
@@ -1,8 +0,0 @@
-function param = clear_basis(param)
-% param = CLEAR_BASIS(param) returns a param struct with all Krylov basis
-% quantities removed.
-
-%%
-fields = {'Hshort', 'Hnext', 'Vshort', 'Vnext', 'E1', 'Em'};
-param = rmfield(param,fields);
-end
